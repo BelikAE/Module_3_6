760b2858f51140df388c0bfc2f92d280aeb990d2
Марина <@DESKTOP-AE40THV>
2024-07-12 22:24:32 +1000

    Выдача 1 и 2 задачи

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string str1 = Console.ReadLine().Replace(" ", "").ToLower();
+            
+            string str2 = "";
+            foreach (char s in str1)
+            {
+                str2 = s + str2;
+
+            }
+            if (str1 == str2)
+            {
+                Console.WriteLine("Предложение палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Предложение не палиндром");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
